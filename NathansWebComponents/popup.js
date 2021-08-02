da22311cd4c7b05758d42d96f578fed560d8e157
BrilliesNate <57934898+BrilliesNate@users.noreply.github.com>
2021-08-02 15:11:14 +0200

    Add files via upload

---
+++ NathansWebComponents/popup.js
@@ -0,0 +1,210 @@
+const template = document.createElement('template');
+template.innerHTML = `
+
+<link rel="stylesheet" href="tooltip.css">
+
+<div class = "tooltip-container">
+<svg
+  
+   viewBox="0 0 6.4339459 6.3885518"
+   version="1.1"
+   id="svg5"
+   sodipodi:docname="open.svg"
+   inkscape:version="1.1 (c68e22c387, 2021-05-23)"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg" class="alert">
+  <sodipodi:namedview
+     id="namedview7"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:document-units="mm"
+     showgrid="false"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     inkscape:zoom="1"
+     inkscape:cx="-9.5"
+     inkscape:cy="-15.5"
+     inkscape:window-width="1920"
+     inkscape:window-height="1017"
+     inkscape:window-x="3832"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="layer1" />
+  <defs
+     id="defs2" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(53.757538,-126.90842)">
+    <g
+       id="g7148"
+       transform="matrix(0.29904194,0,0,0.28695115,-75.185377,100.81736)">
+      <path
+         style="fill:#03a9f4;fill-opacity:1"
+         id="path1416"
+         sodipodi:type="arc"
+         sodipodi:cx="82.41256"
+         sodipodi:cy="102.05686"
+         sodipodi:rx="10.757598"
+         sodipodi:ry="11.131776"
+         sodipodi:start="0.0080110613"
+         sodipodi:end="0.0079237948"
+         sodipodi:arc-type="slice"
+         d="M 93.169812,102.14604 A 10.757598,11.131776 0 0 1 82.326615,113.18828 10.757598,11.131776 0 0 1 71.655303,101.96817 10.757598,11.131776 0 0 1 82.498034,90.925437 10.757598,11.131776 0 0 1 93.16982,102.14507 l -10.75726,-0.0882 z" />
+      <path
+         style="color:#000000;fill:#ffffff;stroke-width:0.222454;-inkscape-stroke:none"
+         d="m 80.356934,93.310463 h 4.298354 c 2e-6,0 3e-6,2e-6 3e-6,4e-6 l -0.882822,11.837943 -0.619734,0.006 -2.795798,0.0117 c -2e-6,0 0.952979,-1e-5 0.952979,-1e-5 L 80.356933,93.310467 c 0,-2e-6 2e-6,-4e-6 4e-6,-4e-6 z"
+         id="rect5887"
+         sodipodi:nodetypes="ssscsscsss" />
+      <path
+         style="fill:#ffffff;fill-opacity:1"
+         id="path6458"
+         sodipodi:type="arc"
+         sodipodi:cx="82.549995"
+         sodipodi:cy="108.69414"
+         sodipodi:rx="2.0174479"
+         sodipodi:ry="2.0009115"
+         sodipodi:start="0.0080110613"
+         sodipodi:end="0.0079237948"
+         sodipodi:arc-type="slice"
+         d="m 84.567379,108.71017 a 2.0174479,2.0009115 0 0 1 -2.033501,1.98482 2.0174479,2.0009115 0 0 1 -2.001266,-2.01679 2.0174479,2.0009115 0 0 1 2.033413,-1.98491 2.0174479,2.0009115 0 0 1 2.001355,2.0167 l -2.017385,-0.0159 z" />
+    </g>
+  </g>
+</svg>
+
+
+
+<svg
+
+   viewBox="0 0 21.515195 22.263552"
+   version="1.1"
+   id="svg5"
+   sodipodi:docname="exit.svg"
+   inkscape:version="1.1 (c68e22c387, 2021-05-23)"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg" class="cancel">
+  <sodipodi:namedview
+     id="namedview7"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:document-units="mm"
+     showgrid="false"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     inkscape:zoom="1"
+     inkscape:cx="-316.5"
+     inkscape:cy="137.5"
+     inkscape:window-width="1920"
+     inkscape:window-height="1017"
+     inkscape:window-x="3832"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="layer1" />
+  <defs
+     id="defs2" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-57.769486,-50.51614)">
+    <g
+       id="g6875"
+       transform="translate(91.545833,-35.983333)">
+      <path
+         style="fill:#03a9f4;fill-opacity:1"
+         id="path1416-0"
+         sodipodi:type="arc"
+         sodipodi:cx="-23.018749"
+         sodipodi:cy="97.631248"
+         sodipodi:rx="10.757598"
+         sodipodi:ry="11.131776"
+         sodipodi:start="0.0080110613"
+         sodipodi:end="0.0079237948"
+         sodipodi:arc-type="slice"
+         d="m -12.261497,97.720425 a 10.757598,11.131776 0 0 1 -10.843196,11.042245 10.757598,11.131776 0 0 1 -10.671313,-11.220112 10.757598,11.131776 0 0 1 10.842732,-11.042734 10.757598,11.131776 0 0 1 10.671785,11.219629 l -10.75726,-0.08821 z" />
+      <rect
+         style="fill:#ffffff;fill-opacity:1;stroke-width:0.215205"
+         id="rect6686"
+         width="2.8442707"
+         height="12.700688"
+         x="-83.713776"
+         y="51.333252"
+         ry="3.6509186e-06"
+         transform="matrix(0.74595489,-0.66599648,0.67373028,0.73897734,0,0)" />
+      <rect
+         style="fill:#ffffff;fill-opacity:1;stroke-width:0.215205"
+         id="rect6870"
+         width="2.8442707"
+         height="12.700688"
+         x="45.112907"
+         y="81.681328"
+         ry="3.6509186e-06"
+         transform="matrix(0.75076458,0.66056986,-0.65271399,0.75760441,0,0)" />
+    </g>
+  </g>
+</svg>
+
+<div class= "notify-container">
+ <slot name= "message"/>
+</div>
+ </div>
+
+`;
+
+
+class PopupNotify extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    tooltip(expandState) {
+        const tooltip = this.shadowRoot.querySelector('.notify-container');
+        const alert = this.shadowRoot.querySelector('.alert');
+        const cancel = this.shadowRoot.querySelector('.cancel');
+
+        if (expandState == true) {
+            tooltip.style.transform = 'scale(1)';
+            alert.style.display = 'none';
+            cancel.style.display = 'block';
+            expandState = false;
+            
+        } else {
+            tooltip.style.transform = 'scale(0)';
+            alert.style.display = 'block';
+            cancel.style.display = 'none';
+
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('.alert').addEventListener('click', () => {
+            this.tooltip(true)
+        })
+        this.shadowRoot.querySelector('.cancel').addEventListener('click', () => {
+            this.tooltip(false)
+        })
+    }
+}
+
+
+
+window.customElements.define('popup-notify', PopupNotify)

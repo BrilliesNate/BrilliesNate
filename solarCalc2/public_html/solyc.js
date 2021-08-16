0dc13d7530e0f0975ac031f018f81f59a7d8a68b
BrilliesNate <57934898+BrilliesNate@users.noreply.github.com>
2021-08-16 16:55:27 +0200

    the new solyc updates

--- solarCalc2/public_html/solyc.js
+++ solarCalc2/public_html/solyc.js
@@ -21,28 +21,142 @@ function getLocation() {
 
 function showPosition(position) {
     lat.innerHTML = position.coords.latitude;
-     long.innerHTML = position.coords.longitude;
+    long.innerHTML = position.coords.longitude;
 }
 
-function getDateTime(){
+function getDateTime() {
     let d = new Date();
 
     // Getting Date Year/Month/Day
-    document.getElementById("year").innerHTML = d.getFullYear();
-    document.getElementById("month").innerHTML = d.getMonth();
-    document.getElementById("day").innerHTML = d.getDate();
+    // document.getElementById("year").innerHTML = d.getFullYear();
+    // document.getElementById("month").innerHTML = d.getMonth() +1;
+    // document.getElementById("day").innerHTML = d.getDate();
     // Getting Time Hours/Minutes/Seconds
     document.getElementById("hour").innerHTML = d.getHours();
     document.getElementById("minutes").innerHTML = d.getMinutes();
     document.getElementById("seconds").innerHTML = d.getSeconds();
 }
 
-function getUtcOffset(){
+function getUtcOffset() {
     let d = new Date();
     let n = d.getTimezoneOffset();
     document.getElementById("UCT").innerHTML = n;
 }
 
+function oriantaion(){
+    let slider = document.getElementById('panelOriantaion');
+    let sliderOutput = document.getElementById('panelBox');
+
+    sliderOutput.innerHTML = slider.value;
+
+}
+let rotateAngle = slider.value;
+
+function rotate (){
+let panel = document.getElementById('path3086')
+
+panel.setAttribute('style', 'transform: rotate')
+
+}
+
+
+
+// function compass(orient) {
+//     var compas;
+//     if ((orient >= 0 && orient <= 11) || (orient >= 349 && orient <= 360)) {
+//         compas = "N";
+//     }
+
+//     if ((orient > 11 && orient <= 33)) {
+//         compas = "NNE";
+//     }
+
+//     if ((orient > 33 && orient <= 56)) {
+//         compas = "NE";
+//     }
+
+//     if ((orient > 56 && orient <= 79)) {
+//         compas = "ENE";
+//     }
+
+//     if ((orient > 79 && orient <= 101)) {
+//         compas = "E";
+//     }
+
+//     if ((orient > 101 && orient <= 124)) {
+//         compas = "ESE";
+//     }
+
+//     if ((orient > 124 && orient <= 146)) {
+//         compas = "SE";
+//     }
+
+//     if ((orient > 146 && orient <= 169)) {
+//         compas = "SSE";
+//     }
+
+//     if ((orient > 169 && orient <= 191)) {
+//         compas = "S";
+//     }
+
+//     if ((orient > 191 && orient <= 214)) {
+//         compas = "SSW";
+//     }
+
+//     if ((orient > 214 && orient <= 236)) {
+//         compas = "SW";
+//     }
+
+//     if ((orient > 236 && orient <= 259)) {
+//         compas = "WSW";
+//     }
+
+//     if ((orient > 259 && orient <= 281)) {
+//         compas = "W";
+//     }
+
+//     if ((orient > 281 && orient <= 303)) {
+//         compas = "WNW";
+//     }
+
+//     if ((orient > 303 && orient <= 326)) {
+//         compas = "NW";
+//     }
+
+//     if ((orient > 326 && orient < 349)) {
+//         compas = "NNW";
+//     }
+
+//     return compas;
+
+// }
+
+// function rotating() {
+
+// }
+
+
+
+// var sliderOrientation = document.getElementById("panelOriantaion");
+// var outputOrientation = document.getElementById("long");
+
+// let panel = document.getElementById('path3086');
+
+// outputOrientation.innerHTML = sliderOrientation.value;
+// sliderOrientation.oninput = function () {
+//     var orient = sliderOrientation.value;
+//     var compas = compass(orient);
+//     compassArrows(orient);
+//     outputOrientation.innerHTML = compas + " - " + orient;
+// }
+
+// function compassArrows(orient, color) {
+//     var b = panel.rotate(orient, panel.x() + 6, panel.y() + 40)
+// }
+
+// let circle = document.getElementById("image2717")
+
+// circle.style.transform = 'rotate:(100deg)';
 
 
 

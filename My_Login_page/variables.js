2dd1c5d84880436220fc17f8d0c38fbb1a857e2e
BrilliesNate <57934898+BrilliesNate@users.noreply.github.com>
2021-09-22 15:54:14 +0200

    Add files via upload
    login page without background

---
+++ My_Login_page/variables.js
@@ -0,0 +1,15 @@
+
+let loginBtn = document.getElementById('login');
+let signupBtn = document.getElementById('signup');
+let sighnUpUserValue = document.getElementById('sighnUpUserInput');
+let sighnUpPasswordValue = document.getElementById('sighnUpPasswordInput');
+
+
+let loginEnterBtn = document.getElementById('enterLogin');
+let userNameValue = document.getElementById('UserNameLoginInput');
+let passwordValue = document.getElementById('passwordLoginInput');
+
+
+let overlayBlock = document.getElementById('overlayScreen');
+let successMsg = document.getElementById('successContainer');
+let errorMsg = document.getElementById('errorContainer');

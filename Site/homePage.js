1dbd6ca9624d15b77375d1153aba262aa943bb82
Brillies Nate <57934898+BrilliesNate@users.noreply.github.com>
2022-01-14 16:48:30 +0200

    Add files via upload

--- Site/homePage.js
+++ Site/homePage.js
@@ -23,6 +23,42 @@ toggleIcon.addEventListener('click', () => {
     }
 });
 
+let number = document.getElementById("number");
+let number2 = document.getElementById("number2");
+let number3 = document.getElementById("number3");
+let number4 = document.getElementById("number4");
+let counter = 0;
+setInterval(() => {
+  if (counter == 65) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.innerHTML = "WEB DEV " + counter + "%";
+  }
+}, 30);
 
+let webDevBtn = document.getElementById("webDevBtn");
+let webDevCard = document.getElementById("webDevCard");
+let gearsGif = document.getElementById("gearsGif");
+let webDevIcon = document.getElementById("webDevIcon");
+
+webDevCard.onmouseover = function(){
+    webDevCard.style.background = "#ADD8E6"
+    webDevCard.style.transition = "all 0.5s";
+    gearsGif.style.display = "block";
+    webDevIcon.style.display = "none";
+
+    webDevIcon.style.transition = "all 0.5s";
+    gearsGif.style.transition = "all 0.5s";
+};
+webDevCard.onmouseout = function(){
+    gearsGif.style.display = "none";
+    webDevIcon.style.display = "block";
+
+    webDevIcon.style.transition = "all 0.5s";
+    gearsGif.style.transition = "all 0.5s";
+    webDevCard.style.background = "white"
+    webDevCard.style.transition = "all 0.3s";
+};
 
 
